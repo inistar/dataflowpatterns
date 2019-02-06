c3be1905e3778e7222f66347e08a7f0bd50d846e
Iniyavan Sathiamurthi <iniyavansathiamurthi@iniyavans-mbp.lan>
2019-02-06 13:11:18 -0800

    adding changinglookupcache file

---
+++ ChangingLookupCache.java
@@ -0,0 +1,15 @@
+package com.springml.dataflow.patterns;
+
+import jdk.jfr.Description;
+
+public class ChangingLookupCache {
+
+    public interface Option PipelineOptions{
+
+        @Description("INPUT Pubsub Topic")
+        String getInputPubsubTopic();
+        void setInputPubsubTopic(String value);
+    }
+
+    private static Ta
+}
