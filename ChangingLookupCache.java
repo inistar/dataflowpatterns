4e8300fd7229eb735e04ae0cf7ba73cf99b72686
Iniyavan Sathiamurthi <iniyavansathiamurthi@iniyavans-mbp.lan>
2019-02-07 10:11:00 -0800

    pubsub working

--- ChangingLookupCache.java
+++ ChangingLookupCache.java
@@ -1,16 +1,20 @@
 package com.springml.dataflow.patterns;
 
 import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.io.GenerateSequence;
+import org.apache.beam.sdk.io.gcp.pubsub.PubsubIO;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.testing.PAssert;
-import org.apache.beam.sdk.transforms.Create;
-import org.apache.beam.sdk.transforms.DoFn;
-import org.apache.beam.sdk.transforms.Min;
-import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.transforms.*;
 import org.apache.beam.sdk.options.Description;
+import org.apache.beam.sdk.transforms.windowing.AfterProcessingTime;
+import org.apache.beam.sdk.transforms.windowing.GlobalWindows;
+import org.apache.beam.sdk.transforms.windowing.Repeatedly;
+import org.apache.beam.sdk.transforms.windowing.Window;
 import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.values.PCollectionView;
+import org.joda.time.Duration;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -36,6 +40,8 @@ public class ChangingLookupCache {
         PCollectionView<Integer> minNumber = processedMeanNumbers.apply(Min.integersGlobally().asSingletonView());
         PCollection<Integer> processedNumbers = pipeline.apply(Create.of(Arrays.asList(1, 2, 3, 4, 5, 6)));
 
+
+
         PCollection<Integer> numbersFromChars = processedNumbers.apply(
                 ParDo.of(new DoFn<Integer, Integer>() {
                     @ProcessElement
@@ -56,7 +62,22 @@ public class ChangingLookupCache {
 
     public static void main(String[] args){
 
-        should_show_side_input_in_global_window(args);
+        Options options = PipelineOptionsFactory.fromArgs(args).withValidation().as(Options.class);
 
+        Pipeline pipeline = Pipeline.create(options);
+
+        PCollection<String> mainStream = pipeline.apply("Read from Pubsub",
+                PubsubIO.readStrings().fromTopic(options.getInputPubsubTopic()));
+
+        mainStream.apply("Print the Data", ParDo.of(new DoFn<String, String>(){
+
+            @ProcessElement
+            public void processElement(ProcessContext c){
+                System.out.println(c.element());
+                c.output("test");
+            }
+        }));
+
+        pipeline.run().waitUntilFinish();
     }
 }
