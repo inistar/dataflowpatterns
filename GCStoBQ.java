495180e5958756c9d2de206a7573d24188fc0d33
Iniyavan Sathiamurthi <iniyavansathiamurthi@iniyavans-mbp.lan>
2019-02-22 09:05:31 -0800

    adding new gcstobq file and updates to airflow

---
+++ GCStoBQ.java
@@ -0,0 +1,93 @@
+package com.springml.dataflow.patterns;
+
+import com.google.api.services.bigquery.model.TableFieldSchema;
+import com.google.api.services.bigquery.model.TableRow;
+import com.google.api.services.bigquery.model.TableSchema;
+import com.google.cloud.bigtable.beam.CloudBigtableIO;
+import com.google.cloud.bigtable.beam.CloudBigtableTableConfiguration;
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.io.GenerateSequence;
+import org.apache.beam.sdk.io.TextIO;
+import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
+import org.apache.beam.sdk.options.Description;
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.values.PCollection;
+import org.apache.hadoop.hbase.client.Mutation;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.util.Bytes;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class GCStoBQ {
+
+    public interface Options extends PipelineOptions {
+
+        @Description("INPUT File")
+        String getInputFile();
+        void setInputFile(String value);
+
+        @Description("OUTPUT BigQuery Dataset")
+        String getOutputDataset();
+        void setOutputDataset(String value);
+
+    }
+
+    private static TableSchema createBigQuerySchema() {
+
+        List<TableFieldSchema> fields = new ArrayList<>();
+        fields.add(new TableFieldSchema().setName("event_id").setType("INT64"));
+        fields.add(new TableFieldSchema().setName("message").setType("STRING"));
+        fields.add(new TableFieldSchema().setName("event_ts").setType("STRING"));
+
+        return new TableSchema().setFields(fields);
+    }
+
+    public static class FormatBigQueryFn extends DoFn<String, TableRow> {
+
+        static long increment = 0;
+
+        @ProcessElement
+        public void processElement(@Element String data, OutputReceiver<TableRow> out){
+            TableRow row = new TableRow();
+
+            Random rand = new Random();
+            int n = rand.nextInt(1000);
+
+            row.set("event_id", n);
+            row.set("message", data);
+            row.set("event_ts", Instant.now().toString());
+
+            out.output(row);
+        }
+    }
+
+    public static void main(String[] args){
+
+        Options options = PipelineOptionsFactory.fromArgs(args).withValidation().as(Options.class);
+
+        Pipeline pipeline = Pipeline.create(options);
+
+        PCollection<String> input = pipeline.apply("Read Files", TextIO.read().from(options.getInputFile()));
+
+        PCollection<TableRow> BQformat = input.apply(ParDo.of(new FormatBigQueryFn()));
+
+//       BIGQUERY
+//       Schema is only necessary for the first time when the table is not created. Change
+//       CREATE_IF_NEEDED if a new table needs to be created and uncomment the schema parts.
+
+        TableSchema schema = createBigQuerySchema();
+        BQformat.apply(BigQueryIO.writeTableRows()
+                .to(options.getOutputDataset())
+                .withSchema(schema)
+                .withWriteDisposition(BigQueryIO.Write.WriteDisposition.WRITE_APPEND)
+                .withCreateDisposition(BigQueryIO.Write.CreateDisposition.CREATE_IF_NEEDED));
+
+        pipeline.run();
+    }
+}
--- airflow/gcstobigquery_airflow.py
+++ airflow/gcstobigquery_airflow.py
@@ -1,4 +1,5 @@
 import json
+import pendulum
 from datetime import timedelta, datetime
 
 from airflow import DAG
@@ -8,7 +9,7 @@ from airflow.contrib.operators.gcs_to_bq import GoogleCloudStorageToBigQueryOper
 default_args = {
     'owner': 'airflow',
     'depends_on_past': True,
-    'start_date': datetime(2019, 2, 14),
+    'start_date': datetime(2019, 2, 16),
     'email': ['ini.sathiamurthi@sprigml.com'],
     'email_on_failure': True,
     'email_on_retry': False,
@@ -21,18 +22,29 @@ schedule_interval = "00 10 * * *"
 dag = DAG(
     'gcstobigquery_airflow',
     default_args=default_args,
-    schedule_interval=schedule_interval
+    # schedule_interval=schedule_interval
 )
 
 BQ_CONN_ID = "my_gcp_conn"
 BQ_PROJECT = "dataflowtesting-218212"
 BQ_DATASET = "testing"
 
+def new_ts_function(context):
+    # print("initest : " + str(context.get("execution_date")))
+    # print("initest : " + type(context.get("execution_date")))
+    # print("initest : " + type(context.get("dag").schedule_interval))
+
+    test = pendulum.datetime(2019, 2, 16, 10, 0, 0)
+    print("initest Execution_date: " + str(context['execution_date']) + " Schedule_interval: " + str(context['dag'].schedule_interval))
+    return context.get("execution_date") + test#context['dag'].schedule_interval
+
+# def execute(self, context):
+#     execution_date = context.get("execution_date")
 t1 = GoogleCloudStorageObjectSensor(
     task_id='gcs_sensor_trigger',
     bucket='dataflow-results-ini',
     object='gs://dataflow-results-ini/airflow-tesing/test.csv',
-    bigquery_conn_id=BQ_CONN_ID,
+    google_cloud_conn_id='gcs_conn',
     dag=dag
 )
 
@@ -48,6 +60,6 @@ t2 = GoogleCloudStorageToBigQueryOperator(
     dag=dag
 )
 
-# t1 >> t2
+t1 >> t2
 # t1
-t2
+# t2
